23a86fde5aeb9536fb48b7480b9ab76c59e5e76d
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-06-08 15:16:15 +0900

    2020-06-08

---
+++ Practice/ex02/src/main/java/com/cookandroid/ex02/MainActivity.java
@@ -0,0 +1,45 @@
+package com.cookandroid.ex02;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText2);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+                intent.putExtra("value",editText.getText().toString());
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK) {
+            String str = data.getStringExtra("return");
+            Toast.makeText(this, str+" 입니다", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
